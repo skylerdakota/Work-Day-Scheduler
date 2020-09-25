dcc5a35633cc7bccd0e6ea556b3fed74783cfa14
Skyler Dakota Lawton <skylerdakota@gmail.com>
2020-09-25 11:41:02 -0400

    input boxes and save buttons added to HTML

---
+++ script.js
@@ -0,0 +1,20 @@
+// Set variables to traverse the DOM and select by ID and Class
+var dateLocation = $("#currentDay")
+
+var now = moment();
+
+
+
+
+
+// Pseudo-code
+// add date to top of calendar app
+// if statement to recognize if an activity is in the past, present or future
+// -- adding and removing classes to go in past present or future
+// -- respond by changing color of block
+// on click event to save activities to local storage
+$(".save").on("click", function() { 
+}
+// -- stores in local storage updates the dom to store keys and values
+// input capability in activity boxes
+
