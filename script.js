eb382fe2b0385bd08217430d1dc8ce659f164f29
Skyler Dakota Lawton <skylerdakota@gmail.com>
2020-09-25 14:09:42 -0400

    added event listener to determine time of day and respond by adding and removing class attributes to show user time

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 // Set variables to traverse the DOM and select by ID and Class
 var dateLocation = $("#currentDay")
-
+var currentTimeBlock = $(".hour")
 var now = moment();
 
-
-
-
+moment().format();
 
 // Pseudo-code
 // add date to top of calendar app
 $("#currentDay").text(now);
-// if statement to recognize if an activity is in the past, present or future
-// -- adding and removing classes to go in past present or future
-// -- respond by changing color of block
+// event listener and if statement to recognize if an activity is in the past, present or future, 
+//adding and removing classes to respond by changing block colors to indicate time to user
+$(now).on("", function () {
+    if (".hour" === now){
+        textarea.attr("class", ".present");
+        textarea.removeAttr("class", ".future");
+        textarea.removeAttr("class", ".past");
+    } else if (".hour" !== now || ".hour" < now){
+        textarea.attr("class", ".past");
+        textarea.removeAttr("class", ".present");
+        textarea.removeAttr("class", ".future");
+    } else{}
+        textarea.attr("class", ".future");
+        textarea.removeAttr("class", ".present");
+        textarea.removeAttr("class", ".past");
+    }
+})
+
 // on click event to save activities to local storage
 $(".save").on("click", function() { 
 };
 // -- stores in local storage updates the dom to store keys and values
 // input capability in activity boxes
+
+
+//Office hour questions:
+// why are there gaps between bars?
+// how do I get rid of border outline on time block?
+// how do I get an input box to remain color when typing in it?
