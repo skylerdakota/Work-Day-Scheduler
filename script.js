0a6c96e697f5b14b2551bdbaf161f817c61c24a2
Skyler Dakota Lawton <skylerdakota@gmail.com>
2020-09-25 11:51:49 -0400

    added time block info on left of input boxes

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ var now = moment();
 
 // Pseudo-code
 // add date to top of calendar app
+$("#currentDay").text(now);
 // if statement to recognize if an activity is in the past, present or future
 // -- adding and removing classes to go in past present or future
 // -- respond by changing color of block
 // on click event to save activities to local storage
 $(".save").on("click", function() { 
-}
+};
 // -- stores in local storage updates the dom to store keys and values
 // input capability in activity boxes
-
